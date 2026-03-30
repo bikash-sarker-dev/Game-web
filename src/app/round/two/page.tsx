4299d486474216fa66e16b5b11586dbf070cf946
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-03-30 22:49:35 +0600

    the quetoin thinkin proccess

---
+++ src/app/round/two/page.tsx
@@ -0,0 +1,11 @@
+import ThinkingProccess from "@/components/navbar/roundOne/thniking/Thinking";
+
+const page = () => {
+  return (
+    <div>
+      <ThinkingProccess />
+    </div>
+  );
+};
+
+export default page;
