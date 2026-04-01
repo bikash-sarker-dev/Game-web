8e963b0ca64557c530c56624dd4472a6804464b3
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-04-01 10:41:57 +0600

    the joining page desing

---
+++ src/app/round-two/round-two-three/page.tsx
@@ -0,0 +1,11 @@
+import BatchJoining from "@/components/roundTwo/BachJuning/TheBachJoining";
+
+const page = () => {
+  return (
+    <div>
+      <BatchJoining />
+    </div>
+  );
+};
+
+export default page;
